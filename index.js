3a222c689f8645e8085730ab12766f6bf1f88442
Sadra Hosseini <sadra.hosseini@torontomu.ca>
2026-07-31 11:07:11 -0400

    the first phase is functioning

--- index.js
+++ index.js
@@ -2,36 +2,54 @@ import { menuArray } from "./data.js";
 
 const menu = document.getElementById("menu-contatiner");
 const order = document.getElementById("orderBox");
+const totalPrice = document.getElementById("total-price-value");
+
+let isCartEmpty = true;
+let total = 0;
+
+let orders = document.getElementById("orders");
 
 menu.addEventListener("click", function(e){
     if(e.target.classList.contains("add-btn")){
         order.innerHTML += addToOrder(e.target.dataset.id)
     }
 
-   
-  
 })
+
+order.addEventListener("click", function(e){
+    if(e.target.dataset.remove === "remove"){
+        e.target.parentElement.parentElement.remove()
+        if(total > 0){
+            total -= 1
+        }
+        if(total === 0){
+            orders.style.display = 'none';
+        }
+    }
+})
+
 
 function addToOrder(id){
     const targetItem = menuArray.find(function(item){
         return item.id == id
 
+
     })
 
     return `
-<div class="orderAndRemove">
-                    <p class="order-name">${targetItem.name}</p>
-                    <button class="remove-btn" data-remove="remove">Remove</button> 
-                </div>  
-                <p class="price-order">${targetItem.price}</p>
-</div>
+    <div class="order-item-info">
+        <div class="orderAndRemove">
+            <p class="order-name">${targetItem.name}</p>
+            <button class="remove-btn" data-remove="remove">Remove</button> 
+        </div>  
+        <p class="price-order">$${targetItem.price}</p>
+    </div>
 
 `
-
-    
-
-
 }
+
+
+
 
 
 
